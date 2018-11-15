34f601b5ee970ad4d68f4086c1b3c1aa54ce7540
Paris Ward <wardparis.pw@gmail.com>
2018-11-14 17:24:07 -0800

    began oauth flow

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-//by calling .get() we are creating a new route handler
-//in this case, requests trying to access '/'
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    accessToken => {
+        console.log(accessToken);
+    }
+  )
+);
 
-//whenever Heroku runs our app, it has the ability to inject environment variables(provess.env).
-//environment variables are variables that are set in the underlying runtime that node runs on top of.
-//to keep hosting in a development environment i.e. local host, we add a boolean statement
+app.get(
+    '/auth/google',
+     passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+//environment variables(process.env) are variables that are set in the underlying runtime.
+//to keep hosting in a development environment i.e. local host, we add a boolean statement( || 5000).
 const PORT = process.env.PORT || 5000;
-//.listen() instructs express to tell node to listen
-//for incoming traffic on the specified port 
 app.listen(PORT);
