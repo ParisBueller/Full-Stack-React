5bba964890dc23533367b6320b874c35f63bce4b
Paris Ward <wardparis.pw@gmail.com>
2018-11-15 09:32:34 -0800

    refactoring and cleanup

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, 
-    accessToken => {
-        console.log(accessToken);
-    }
-  )
-);
-
-app.get(
-    '/auth/google',
-     passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 //environment variables(process.env) are variables that are set in the underlying runtime.
 //to keep hosting in a development environment i.e. local host, we add a boolean statement( || 5000).
