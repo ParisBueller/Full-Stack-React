bbe7a8d2770e1aa5dd30591fe25a5d11ecb5a79d
Paris Ward <wardparis.pw@gmail.com>
2019-01-25 10:28:18 -0800

    added serializing and deserializing of users, enabled cookie usage for OAuth

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-
+//connect to our mongo database using mongoose.connect()
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+      //maxAge = how long before cookie expires
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    //key = used to encrypt our cookie
+    keys: [keys.cookieKey] 
+  })  
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,24 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+//serializeUser will generate an identifying piece of info to be set
+//in a cookie for the user to use while they navigate around the site
+//serializeUser takes a callback with two arguments, our user model and done argument
+passport.serializeUser((user, done) => {
+    //user.id IS NOT the profile.id,it is a unique identifier
+    //assigned by mongo when the record was created
+    done(null, user.id);
+});
+
+//deserializeUser takes a callback with arguments id and done
+//deserializeUser turns the id back into a mongoose model instance
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -12,8 +30,20 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id }).save();
- 
-    }
-  )
+        //.findOne is a mongoose method used to find one specific document
+        User.findOne({ googleId: profile.id })
+            .then( existingUser => {
+                //we already have a user with the given id
+                if (existingUser) {
+                    //done expects two arguments, null and user record
+                    done(null,existingUser);
+                } else {
+                    //we do not have a record for this user, make a new record
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
+        }
+    )
 );
