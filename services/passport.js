ba10e68769942a084ec3c9be4a6ba1df87a2f3e0
Paris Ward <wardparis.pw@gmail.com>
2019-01-29 11:39:50 -0800

    added proxy to google strategy

--- services/passport.js
+++ services/passport.js
@@ -27,7 +27,8 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
     }, 
     (accessToken, refreshToken, profile, done) => {
         //.findOne is a mongoose method used to find one specific document
