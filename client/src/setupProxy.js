e16a4574c76ab3545adcd544f7a5af65acb0664d
Paris Ward <wardparis.pw@gmail.com>
2019-01-31 09:39:33 -0800

    setup proxy middleware for forwarding of relative links in front end to back end

---
+++ client/src/setupProxy.js
@@ -0,0 +1,10 @@
+//setup proxy is a way to use relative links on the front end i.e. /auth/google
+//and not have to worry about the domain by forwarding request to our back end
+//express server. In production, we DO NOT need to change the target: to
+//our deployed domain because in production, react's development server doesn't exist
+
+const proxy = require('http-proxy-middleware');
+ 
+module.exports = function(app) {
+    app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
+};
