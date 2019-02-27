c8f94a28c4c07417b711ad7b34a7edf7ea5054fe
Paris Ward <wardparis.pw@gmail.com>
2019-02-27 09:26:01 -0800

    survey creation, sendgrid setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,23 @@
+const mongoose = require('mongoose');
+
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
+
 //we must add our arguments to route handlers IN THE ORDER WE
 //WANT THEM TO BE EXECUTED!!
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+        const { title, subject, body, recipients } = req.body;
 
+        const survey = new Survey({
+           title,
+           subject,
+           body,
+           recipients: recipients.split(',').map( email => ({ email: email.trim() })),
+           _user: req.user.id,
+            dateSent: Date.now()
+        });
     });
 };
