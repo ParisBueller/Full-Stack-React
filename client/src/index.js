d6b9c7fb6f61e48bc66760eeb11b80aeda826267
Paris Ward <wardparis.pw@gmail.com>
2019-03-01 10:08:28 -0800

    Mailer routes, mailer on-click redirects

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
+
 import App from './components/App';
 import reducers from './reducers';
+//development only axios helper!
+import axios from 'axios';
+window.axios = axios;
 
 //create a new instance of our redux-store
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,13 @@ const Survey = mongoose.model('surveys');
 //we must add our arguments to route handlers IN THE ORDER WE
 //WANT THEM TO BE EXECUTED!!
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    // vote redirect
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!');
+    });
+    
+    //post request to send emails
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
         const survey = new Survey({
@@ -23,5 +29,20 @@ module.exports = app => {
         });
 
         const mailer = new Mailer(survey, surveyTemplate(survey));
+        try {
+            //await our emails to be sent
+            await mailer.send();
+            //save the survey to the database
+            await survey.save();
+            //subtract credits from the user
+            req.user.credits -= 1;
+            //save the users new info i.e. new credit count
+            const user = await req.user.save();
+            //send back user model to indicate new credit count
+            res.send(user);
+        } catch (err) {
+            //422 = unprocessable entity i.e. you did something wrong
+            res.status(422)
+        }
     });
 };
