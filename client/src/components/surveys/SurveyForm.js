c2556588bae40f240d874a8f1e793d59fe6efc5b
Paris Ward <wardparis.pw@gmail.com>
2019-03-07 09:42:04 -0800

    form styling, form validation

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,16 +1,34 @@
 //SurveyForm shows a form for a user to add input
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 import SurveyField from './SurveyField';
+//Fields array to customize Field tags to keep from bloating JSX
+const FIELDS = [
+    {label: 'Survey Title', name: 'title'},
+    {label: 'Subject Line', name: 'subject'},
+    {label: 'Email Body', name: 'body'},
+    {label: 'Recipient List', name: 'emails'}
+];
 
 class SurveyForm extends Component {
     renderFields() {
-        return (
-            <div>
-                <Field type="text" name="title" component={SurveyField} />
-            </div>
-        );
+        //iterate over our list of above defined FIELDS array with lodash
+        // and for every object in there return a custom Redux form <Field /> 
+        //with custom label and name properties
+        return _.map(FIELDS, ({label, name}) => {
+            return (
+                <Field 
+                    key={name} 
+                    component={SurveyField} 
+                    type="text" 
+                    label={label} 
+                    name={name}
+                />
+            );
+        });
     }
 
     render() {
@@ -18,15 +36,43 @@ class SurveyForm extends Component {
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text">
+                    Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                    Next
+                    <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
     }
 }
+//function that validates our form fields
+//takes one argument:values object containing all the 
+//different values coming from our form
+//Redux-Form automatically matches up the errors you return
+// to the different fields you are rendering
+function validate(values) {
+     const errors = {};
+    //for each name property in the FIELDS array
+    //check if a value was provided, if not 
+    //return an error message prompting an input value
+     _.each(FIELDS, ({ name }) => {
+         if (!values[name]) {
+             errors[name] = 'You must provide a value'
+         }
+     });
+     //if our errors object remains empty, proceed to submit
+     return errors;
+}
+
 //reduxForm takes one single argument that
 //contains options to customize form behavior
-//only REQUIRES one option:: form
+//only REQUIRES one option:: form.
+//we can also pass the validate option and pass in a function
+//that validates the fields every time the user submits the form
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm);
